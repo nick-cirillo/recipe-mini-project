fae2366d0c4d6e3e89872f5896e14cde016e35a6
nick-cirillo <77818146+nick-cirillo@users.noreply.github.com>
2022-12-02 17:16:37 -0500

    initial commit

---
+++ main/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from .models import Tweet
+from datetime import datetime
+
+def main_view(request):
+    if not request.user.is_authenticated:
+        return redirect('/splash/')
+    if request.method == 'POST' and request.POST['body'] != "":
+        tweet = Tweet.objects.create(
+            body = request.POST['body'],
+            author = request.user,
+            created_at = datetime.now()
+        )
+        tweet.save()
+    tweets = Tweet.objects.order_by('-created_at')
+    return render(request, 'main.html', {'tweets': tweets})
+
+def splash_view(request):
+    return render(request, 'splash.html')
+
+def login_view(request):
+    username, password = request.POST['username'], request.POST['password']
+
+    user = authenticate(username=username, password=password)
+    
+    if user is not None:
+        login(request, user)
+        return redirect('/')
+    else:
+        return redirect('/splash?error=LoginError')
+
+def signup_view(request):
+    user = User.objects.create_user(
+        username=request.POST['username'],
+        password=request.POST['password'],
+        email=request.POST['email'],
+    )
+    login(request, user)
+    return redirect('/')
+
+def logout_view(request):
+    logout(request)
+    return redirect('/splash')
+
+def delete_view(request):
+    tweet = Tweet.objects.get(id = request.GET['id'])
+    if request.user == tweet.author:
+        tweet.delete()
+    return redirect('/')
+
+def like_view(request):
+    tweet = Tweet.objects.get(id = request.GET['id'])
+    
+    if len(tweet.likes.filter(username=request.user.username)) == 0:
+        tweet.likes.add(request.user)
+    else:
+        tweet.likes.remove(request.user)
+    tweet.save()
+    return redirect('/')
